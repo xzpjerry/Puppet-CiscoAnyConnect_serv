7092268bca77dfed25b52761bbfc08d448d814dd
Jerry Xie <jerryxie@JMBP.local>
2019-07-11 16:14:17 -0700

    to see echo output

--- code/modules/sshd/manifests/action.pp
+++ code/modules/sshd/manifests/action.pp
@@ -1,6 +1,7 @@
 class sshd::action {
 	exec { 'testing':
-        command => 'for filename in puppet:///modules/sshd/*.pub; do echo $filename; done',
+        command => 'for filename in puppet:///modules/sshd/*.pub; do /bin/echo $filename; done',
         provider => shell,
+        logoutput => true ,
     }	
 }
