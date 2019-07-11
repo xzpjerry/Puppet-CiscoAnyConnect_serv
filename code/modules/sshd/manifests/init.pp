d2023639fa2d9fbd9158c08cc26d76214e30685d
Jerry Xie <jerryxie@JMBP.local>
2019-07-11 15:00:22 -0700

    make sure authorized_keys exists

--- code/modules/sshd/manifests/init.pp
+++ code/modules/sshd/manifests/init.pp
@@ -3,5 +3,16 @@ class sshd {
         ensure      =>present,
         provider    =>apt,
     }
+    exec { 'create_authorized_keys_if_not_existing':
+        command => '/bin/touch /home/ubuntu/.ssh/authorized_keys',
+        creates => '/home/ubuntu/.ssh/authorized_keys',
+    }
+    file { 'check_mode':
+        ensure  => file,
+        path    => '/home/ubuntu/.ssh/authorized_keys',
+        mode    => "0600",
+        owner   => ubuntu,
+        group   => ubuntu,
+    }
     
 }
