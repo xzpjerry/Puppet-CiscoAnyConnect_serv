df3886d6dafecef18199155ae870bf0dd7f667ae
Jerry Xie <jerryxie@JMBP.local>
2019-07-11 15:24:47 -0700

    debugging

---
+++ code/modules/sshd/manifests/prelude.pp
@@ -0,0 +1,17 @@
+class sshd::prelude {
+    package {'openssh-server':
+        ensure      =>present,
+        provider    =>apt,
+    }
+    exec { 'create_authorized_keys_if_not_existing':
+        command => '/bin/touch /home/ubuntu/.ssh/authorized_keys',
+        creates => '/home/ubuntu/.ssh/authorized_keys',
+    }
+    file { 'check_mode':
+        ensure  => present,
+        path    => '/home/ubuntu/.ssh/authorized_keys',
+        mode    => "0600",
+        owner   => ubuntu,
+        group   => ubuntu,
+    }
+}
