8459b89248ee8d89e39f93952e5a3918bc86a3ee
m-mmaka <mmaka@uoregon.edu>
2019-07-15 14:07:40 -0700

    Updating

--- code/modules/sshd/manifests/init.pp
+++ code/modules/sshd/manifests/init.pp
@@ -2,4 +2,12 @@ class sshd {
 	package { "openssh-server":
 		ensure => installed
 	}
+
+	file { "etc/ssh/sshd_config":
+		ensure => present,
+		mode => '444',
+		owner => 'root',
+		group => 'root',
+		source => 'puppet:///modules/sshd/sshd_config",
+	}
 }
