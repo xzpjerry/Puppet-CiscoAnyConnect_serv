130eb855d128f1f0fa9ed4f8f730163aaf6ccd14
m-mmaka <mmaka@uoregon.edu>
2019-07-16 13:27:37 -0700

    Changes

--- code/modules/self_update/manifests/init.pp
+++
@@ -1,7 +0,0 @@
-class self_update {
-    cron { 'puppet-apply':
-        command => "cd /etc/puppet && git pull && puppet apply -t manifests/site.pp",
-        user    => root,
-        minute  => "*/5",
-    }
-}
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node ip-10-0-6-204 {
 	include puppet
 	include sshd
-	include cron_puppet
-	include self_update
 }
 
