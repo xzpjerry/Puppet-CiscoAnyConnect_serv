78d461795c412c81cd776ba9067b7ec256e2ea07
Jerry Xie <xzpjerry@gmail.com>
2019-07-16 13:51:07 -0700

    give more documentation to be rational

--- code/modules/sshd/manifests/init.pp
+++ code/modules/sshd/manifests/init.pp
@@ -2,6 +2,8 @@ class sshd {
 	contain sshd::make_sure_package_and_service_are_good
 	contain sshd::ssh_pub_key_list_to_be_imported
 
+	# import keys after packages are installed
+	# and services are running
 	Class['sshd::make_sure_package_and_service_are_good']
 	-> Class['sshd::ssh_pub_key_list_to_be_imported']
 }
