0dfe5601478e83da48d00eaae6358ff239fd41dd
Jerry Xie <jerryxie@JMBP.local>
2019-07-16 17:24:10 -0700

    assignment2 pageserver managed by puppet beta

--- code/modules/pageserver_jerryx/manifests/html_resources_to_be_imported.pp
+++ code/modules/pageserver_jerryx/manifests/html_resources_to_be_imported.pp
@@ -1,3 +1,26 @@
 class pageserver_jerryx::html_resources_to_be_imported {
-	
+	file { '/var/www/html/':
+        ensure => directory,
+        mode    => "0755",
+        owner   => root,
+        group   => root,
+        before => [
+            File['/var/www/html/index.html'],
+            File['/var/www/html/index.css'],
+      ],
+    }
+    file { '/var/www/html/index.html':
+        ensure => file,
+        source => 'puppet:///modules/pageserver_jerryx/index.html',
+        mode    => "0664",
+        owner   => root,
+        group   => root,
+    }
+    file { '/var/www/html/index.css':
+        ensure => file,
+        source => 'puppet:///modules/pageserver_jerryx/index.css',
+        mode    => "0664",
+        owner   => root,
+        group   => root,
+    }
 }
