fc421852ffd9159b064f27b739b73022630af22a
m-mmaka <mmaka@uoregon.edu>
2019-07-22 12:41:29 -0700

    Refreshing

--- code/modules/pageserver_jerryx/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class pageserver_jerryx {
-	contain pageserver_jerryx::make_sure_package_and_service_are_good
-	contain pageserver_jerryx::html_resources_to_be_imported
-
-	# import html resources after packages are installed
-	# and services are running
-	Class['pageserver_jerryx::make_sure_package_and_service_are_good']
-	-> Class['pageserver_jerryx::html_resources_to_be_imported']
-}
