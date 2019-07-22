67d0146746b543e251e5485caa415f831f5b307d
m-mmaka <mmaka@uoregon.edu>
2019-07-21 17:20:00 -0700

    Added apache module

---
+++ code/modules/apache2/manifests/init.pp
@@ -0,0 +1,22 @@
+class apache2 {
+	package { "apache2":
+		ensure => installed;
+	}
+
+	file {"/etc/apache2/apache2.conf":
+		ensure => present,
+		mode => '444',
+		owner => 'root',
+		group => 'root',
+		source => "puppet:///modules/apache2/apache2.conf",
+		require => Package["apache2"],
+	}
+
+	service { "apache2":
+		enable => true,
+		ensure => running,
+		subscribe => File["/etc/apache2/apache2.conf"],
+		require => [Package["apache2"],],
+	}
+
+}
