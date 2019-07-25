aa3c719fa7f571cdd67820b17eee2cefbdc650b0
Jerry Xie <xzpjerry@gmail.com>
2019-07-25 16:39:43 -0700

    possible a name conflict

--- code/modules/user_management/manifests/init.pp
+++ code/modules/user_management/manifests/init.pp
@@ -24,13 +24,13 @@ class user_management {
     'Jerry' => 
       {
         'key' => "AAAAB3NzaC1yc2EAAAADAQABAAACAQC1UoDoKsyxL2O6Ugw2WUr7FxupWnuDI7r23sDCoGQHWW60BzHJUR1ZqTk+qdqA7UuInHj/rn6jSoEV2N/jw/cMNb3GKF9cpVnHnXXnf0dwBtKEH0J4IHD/koZZqc88iUqb9fMZ8YlOSDrIakwAuryFK8bhgDmWqZOGZlPZn9WYkMuLIY4ICwqpr3Snes25N1Mac4hKHY3Y8jTsQLQUm478Kx+m4jnpt85Ymfbvzf6LrleNzNppFwA3mWPcpa9T4qhnlpx789k71j5MboAmK5fUQeFxwwcV04/MO8zcZhjqpYAo7noLh6KopwErDYni6LEkNxwOJvjHZIa1dLKJ1jwSodvjDJV2+tDpppdLWD4XSnA21cZq2BD2Sx0uifv9N8TlAbGcvQasJOMiNXfJ3M6m2UQWMMQ3HlXviko7NzNj925CHz+rBIlVhJmt5d6NsvpIiI8D/hQC0ugclLVDZ6jHrCBu9MdB60cZEbzXP6+h9xltapmw5ikF05SuvKP7Mskor5twLoyq59NQi2iTcFzkQOLDzNzi557EquEQNC4yxzdNr9ZmtepVu2fOuY+djq+UKXp9KuLsAfC6sfjBckb5uZCFYEw+LYZMYJwduyPMIUuP/09ffNWzfptYNa2tZ1FPRtcm9GdjlsCufFoXir3cee25T329HTLcPIfxFKx5GQ==",
-        'type' => "ssh-rsa",
+        'key_type' => "ssh-rsa",
       },
   }
   $user_key_dict.each | String $user_name, Hash $user_info | {
     user_management::myuser { $user_name:
       ssh_key => $user_info[key],
-      ssh_key_type => $user_info[type],
+      ssh_key_type => $user_info[key_type],
     }
   }
 
