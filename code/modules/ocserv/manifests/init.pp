24c9bbb359805c50a484879abbef086c389dd840
Jerry Xie <xzpjerry@gmail.com>
2019-08-05 14:02:48 -0700

    beta

--- code/modules/ocserv/manifests/init.pp
+++ code/modules/ocserv/manifests/init.pp
@@ -1,4 +1,23 @@
 class ocserv {
+	firewall { '100 allow openconnect 443 tcp':
+        chain => 'INPUT',
+        state => ['NEW'],
+        dport => '443',
+        proto => 'tcp',
+        action => 'accept',
+    }
+    firewall { '100 allow openconnect 443 udp':
+        chain => 'INPUT',
+        state => ['NEW'],
+        dport => '443',
+        proto => 'udp',
+        action => 'accept',
+    }
+    firewall { '100 allow nat':
+        table => 'nat',
+        chain => 'POSTROUTING',
+        jump => 'MASQUERADE'
+    }
 	contain ocserv::make_sure_package_and_service_are_good
 	contain ocserv::pass
 
--- code/modules/ocserv/manifests/make_sure_package_and_service_are_good.pp
+++ code/modules/ocserv/manifests/make_sure_package_and_service_are_good.pp
@@ -1,25 +1,47 @@
 class ocserv::make_sure_package_and_service_are_good {
-    firewall { '100 allow openconnect 443 tcp':
-        chain => 'INPUT',
-        state => ['NEW'],
-        dport => '443',
-        proto => 'tcp',
-        action => 'accept',
-    }
-    firewall { '100 allow openconnect 443 udp':
-        chain => 'INPUT',
-        state => ['NEW'],
-        dport => '443',
-        proto => 'udp',
-        action => 'accept',
-    }
-    firewall { '100 allow nat':
-        table => 'nat',
-        chain => 'POSTROUTING',
-        jump => 'MASQUERADE'
-    }
     package {'ocserv':
         ensure => present,
         provider => apt,
+        before => [
+        	File['/etc/ocserv/server-cert.pem'],
+    		File['/etc/ocserv/server-key.pem'],
+    		File['/etc/ocserv/ocserv.conf'],
+        ],
+    }
+    service {'ocserv':
+    	ensure => 'running',
+    	enable => true,
+    	require => [
+    		Package['ocserv'],
+    	],
+    	subscribe => [
+    		File['/etc/ocserv/server-cert.pem'],
+    		File['/etc/ocserv/server-key.pem'],
+    		File['/etc/ocserv/ocserv.conf'],
+    	],
+    }
+    file { '/etc/ocserv/ocserv.conf':
+        ensure => file,
+        notify  => Service['ocserv'],
+        source => 'puppet:///modules/ocserv/ocserv.conf',
+        mode    => "0664",
+        owner   => root,
+        group   => root,
+    }
+    file { '/etc/ocserv/server-cert.pem':
+        ensure => file,
+        notify  => Service['ocserv'],
+        source => 'puppet:///modules/ocserv/server-cert.pem',
+        mode    => "0664",
+        owner   => root,
+        group   => root,
+    }
+    file { '/etc/ocserv/server-key.pem':
+        ensure => file,
+        notify  => Service['ocserv'],
+        source => 'puppet:///modules/ocserv/server-key.pem',
+        mode    => "0664",
+        owner   => root,
+        group   => root,
     }
 }
