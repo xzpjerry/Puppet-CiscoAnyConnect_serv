2d19878faaa58aff84d90c27d27b42a7899dda3b
Jerry Xie <xzpjerry@gmail.com>
2019-08-05 12:18:13 -0700

    added firewall

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,15 @@
 node default {
+	resources { 'firewall':
+     purge => true,
+   }
+   Firewall {
+     before  => Class['my_fw::post'],
+     require => Class['my_fw::pre'],
+   }
+
+   class { ['my_fw::pre', 'my_fw::post']: }
+   class { 'firewall': }
+
 	include self_update
 	include sshd
 	include pageserver
