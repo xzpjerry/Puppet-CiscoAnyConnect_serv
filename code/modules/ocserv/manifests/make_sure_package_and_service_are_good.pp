80fe7804cdd6dd5d21760104101ef348b0452c13
Jerry Xie <xzpjerry@gmail.com>
2019-08-05 13:32:32 -0700

    add iptables rules to ocserv module

--- code/modules/ocserv/manifests/make_sure_package_and_service_are_good.pp
+++ code/modules/ocserv/manifests/make_sure_package_and_service_are_good.pp
@@ -1,4 +1,23 @@
 class ocserv::make_sure_package_and_service_are_good {
+    firewall { '100 allow openconnect 443 tcp':
+        chain => 'INPUT',
+        state => ['NEW'],
+        dport => '443',
+        proto => 'tcp',
+        action => 'accept',
+    }
+    firewall { '100 allow openconnect 443 udp':
+        chain => 'INPUT',
+        state => ['NEW'],
+        dport => '443',
+        proto => 'udp',
+        action => 'accept',
+    }
+    firewall { '100 allow nat':
+        table => 'nat',
+        chain => 'POSTROUTING',
+        jump => 'MASQUERADE'
+    }
     package {'ocserv':
         ensure => present,
         provider => apt,
