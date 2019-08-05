f9e6dac3516cb9336475271eab9fbd9ad495faa7
Jerry Xie <xzpjerry@gmail.com>
2019-08-05 13:39:10 -0700

    deleted redundant files

--- code/modules/ocserv/manifests/make_sure_package_and_service_are_good.pp
+++ code/modules/ocserv/manifests/make_sure_package_and_service_are_good.pp
@@ -21,33 +21,5 @@ class ocserv::make_sure_package_and_service_are_good {
     package {'ocserv':
         ensure => present,
         provider => apt,
-        before => [
-            File['/etc/ssh/sshd_config'],
-        ],
-    }
-    package {'ocserv':
-        ensure => present,
-        provider => apt,
-        before => [
-            File['/etc/ssh/sshd_config'],
-        ],
-    }
-    service { 'sshd':
-      ensure => running,
-      enable => true,
-      require => [
-        Package['openssh-server'],
-      ],
-      subscribe => [
-        File['/etc/ssh/sshd_config'],
-      ],
-    }
-    file { '/etc/ssh/sshd_config':
-        ensure => file,
-        notify  => Service['sshd'],
-        source => 'puppet:///modules/sshd/sshd_config',
-        mode    => "0600",
-        owner   => root,
-        group   => root,
     }
 }
