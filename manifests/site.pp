a6485aa20b3b72f9f0031f80db8a2627d0875ef2
Jerry Xie <xzpjerry@gmail.com>
2019-08-07 20:17:27 -0700

    beta

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,40 @@
+resources { 'firewall':
+ purge => true,
+}
+Firewall {
+ before  => Class['my_fw::post'],
+ require => Class['my_fw::pre'],
+}
+
+class { ['my_fw::pre', 'my_fw::post']: }
+class { 'firewall': }
 node default {
-	include resolvconf
-	resources { 'firewall':
-     purge => true,
-	}
-	Firewall {
-	 before  => Class['my_fw::post'],
-	 require => Class['my_fw::pre'],
-	}
-
-	class { ['my_fw::pre', 'my_fw::post']: }
-	class { 'firewall': }
 	include self_update
-	include sshd
-	include pageserver
-	include user_management
+	include sshd	
+	include resolvconf
 	include ocserv
+}
+node ip-10-0-9-123 {
+	include self_update
 
+	include sshd	
+
+	include pageserver
+
+	include user_management
+
+	include resolvconf
+	include ocserv
+}
+node ip-10-0-9-29 {
+	include self_update
+
+	include sshd	
+
+	include pageserver
+
+	include user_management
+	
+	include resolvconf
+	include ocserv
 }
