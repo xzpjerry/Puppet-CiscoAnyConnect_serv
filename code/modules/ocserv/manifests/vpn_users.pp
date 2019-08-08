8ba8964ca6ca4dd720e1500f1bb95822ba11d1bf
Jerry Xie <xzpjerry@gmail.com>
2019-08-07 20:38:30 -0700

    updatd testing vpn users

--- code/modules/ocserv/manifests/vpn_users.pp
+++ code/modules/ocserv/manifests/vpn_users.pp
@@ -3,11 +3,18 @@ class ocserv::vpn_users {
 		ensure => present,
 		gid => 6007,
 	}
-    user { 'vpn_test':
+    user { 'zhipengx':
     	ensure => present,
     	managehome => false,
     	comment => 'Puppet managed VPN user',
-    	password => '$1$Sug4n39p$1pozT4fLM0IPAliorVJZG/',
+    	password => '$1$Jf3m.KJs$EDMExKARj/0I55c0uwtUP1',
     	groups => ['vpn-users'],
+    }
+    user { 'stevev':
+        ensure => present,
+        managehome => false,
+        comment => 'Puppet managed VPN user',
+        password => '$1$b2X2kmkL$PT33u8Pa2aB5FWq3ftGUJ0',
+        groups => ['vpn-users'],
     }
 }
